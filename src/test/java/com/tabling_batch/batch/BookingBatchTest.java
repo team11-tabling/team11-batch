cfce3086ab7c3c2273b22efc1ee58006a7a4376e
beunchoi <154890316+beunchoi@users.noreply.github.com>
2024-06-18 12:31:47 +0900

    Update BookingBatchTest.java

--- src/test/java/com/tabling_batch/batch/BookingBatchTest.java
+++ src/test/java/com/tabling_batch/batch/BookingBatchTest.java
@@ -1,42 +1,42 @@
-//package com.tabling_batch.batch;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.batch.core.BatchStatus;
-//import org.springframework.batch.core.Job;
-//import org.springframework.batch.core.JobExecution;
-//import org.springframework.batch.core.JobParameters;
-//import org.springframework.batch.core.JobParametersBuilder;
-//import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-//import org.springframework.batch.test.JobLauncherTestUtils;
-//import org.springframework.batch.test.context.SpringBatchTest;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBatchTest
-//@SpringBootTest
-//@EnableBatchProcessing
-//class BookingBatchTest {
-//
-//  @Autowired
-//  private JobLauncherTestUtils jobLauncherTestUtils;
-//
-//  @Autowired
-//  private Job bookingJob;
-//
-//  @Test
-//  public void testBookingJob() throws Exception {
-//    // Given
-//    JobParameters jobParameters = new JobParametersBuilder()
-//        .addString("date", "2024-05-09")
-//        .toJobParameters();
-//
-//    // When
-//    jobLauncherTestUtils.setJob(bookingJob);
-//    JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
-//
-//    // Then
-//    assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
-//  }
-//}
+package com.tabling_batch.batch;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBatchTest
+@SpringBootTest
+@EnableBatchProcessing
+class BookingBatchTest {
+
+ @Autowired
+ private JobLauncherTestUtils jobLauncherTestUtils;
+
+ @Autowired
+ private Job bookingJob;
+
+ @Test
+ public void testBookingJob() throws Exception {
+   // Given
+   JobParameters jobParameters = new JobParametersBuilder()
+       .addString("date", "2024-05-09")
+       .toJobParameters();
+
+   // When
+   jobLauncherTestUtils.setJob(bookingJob);
+   JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+   // Then
+   assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+ }
+}
